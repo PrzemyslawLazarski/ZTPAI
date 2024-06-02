8e9423efb4344e70ee9e59e6b22fe38a22884899
Przemek <przemyslaw.lazarski@student.pk.edu.pl>
2024-06-02 15:45:00 +0200

    Works clear

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import HowItWorks from './pages/How-it-works';
-import AboutUs from './pages/Aboutus';
-import Features from './pages/Features';
-import Dashboard from './pages/Dashboard';
-import MyQuizzes from './pages/My-quizzes';
-import Discover from './pages/Discover';
-import AddQuiz from './pages/AddQuiz';
-import QuizGame from './pages/QuizGame';
-import Profile from './pages/Profile';
-import QuizInfo from './pages/QuizInfo'; 
-import AdminPanel from './pages/AdminPanel'; 
+import Home from './pages/Home/Home';
+import Login from './pages/Security/Login';
+import Register from './pages/Security/Register';
+import HowItWorks from './pages/Home/How-it-works';
+import AboutUs from './pages/Home/Aboutus';
+import Features from './pages/Home/Features';
+import Dashboard from './pages/Dashboard/Dashboard';
+import MyQuizzes from './pages/Dashboard/My-quizzes';
+import Discover from './pages/Dashboard/Discover';
+import AddQuiz from './pages/Quiz/AddQuiz';
+import QuizGame from './pages/Quiz/QuizGame';
+import Profile from './pages/Dashboard/Profile';
+import QuizInfo from './pages/Quiz/QuizInfo'; 
+import AdminPanel from './pages/Dashboard/AdminPanel'; 
 
 function App() {
   return (
